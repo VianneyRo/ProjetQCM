5c489b548d2eaf32c373a6ed1e489e462b0f96c9
mpaviza2017 <mpaviza2017@1-UC54-11.ad.campus-eni.fr>
2018-10-18 09:32:00 +0200

    fix

--- src/main/java/fr/eni/projet/qcm/bo/Profil.java
+++ src/main/java/fr/eni/projet/qcm/bo/Profil.java
@@ -2,8 +2,6 @@ package fr.eni.projet.qcm.bo;
 
 
 public class Profil {
-	
-	private Integer id;
 	private String code;
 	private String libelle;	
 
@@ -13,30 +11,17 @@ public class Profil {
 	public Profil() {}
 
 	/**
-	 * Constructeur BO
+	 * Constructeur BO / BDD
 	 * @param libelle
 	 */
 	public Profil(String code, String libelle) {
 		this.code = code;
 		this.libelle = libelle;
 	}
-	
-	/**
-	 * Constructeur BDD
-	 * @param id
-	 * @param libelle
-	 */
-	public Profil(Integer id, String code, String libelle) {
-		this.id = id;
-		this.code = code;
-		this.libelle = libelle;
-	}
 
-	public Integer getId() { return id; }
 	public String getCode() { return code; }
 	public String getLibelle() { return libelle; }
 
-	public void setId(Integer id) { this.id = id; }
 	public void setCode(String code) { this.code = code; }
 	public void setLibelle(String libelle) { this.libelle = libelle; }
 
